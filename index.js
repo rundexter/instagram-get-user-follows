e4b472a9a93e1f7dceb861d99e3e2c3e1d2eb149
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-20 16:07:18 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -1,113 +1,18 @@
-var _ = require('lodash');
-var ig = require('instagram-node').instagram();
+var _ = require('lodash'),
+    util = require('./util.js'),
+    instagram = require('instagram-node').instagram();
 
-var globalPickResult = {
-    users: {
-        fields: {
-            'id': 'id',
-            'full_name': 'full_name',
-            'profile_picture': 'profile_picture',
-            'username': 'username'
-        }
-    }
-};
+var pickInputs = {
+        'userId': 'userId'
+    },
+    pickOutputs = {
+        'id': { key: 'data', fields: ['id'] },
+        'full_name': { key: 'data', fields: ['full_name'] },
+        'profile_picture': { key: 'data', fields: ['profile_picture'] },
+        'username': { key: 'data', fields: ['username'] }
+    };
 
 module.exports = {
-
-    /**
-     * Return pick result.
-     *
-     * @param output
-     * @param pickTemplate
-     * @returns {*}
-     */
-    pickResult: function (output, pickTemplate) {
-        var result = {};
-        // map template keys
-        _.map(_.keys(pickTemplate), function (templateKey) {
-
-            var oneTemplateObject = pickTemplate[templateKey];
-            var outputKeyValue = _.get(output, templateKey);
-
-            if (!outputKeyValue) {
-
-                return result;
-            }
-            // if template key is object - transform, else just save
-            if (_.isObject(oneTemplateObject)) {
-                // if data is array - map and transform, else once transform
-                if (_.isArray(outputKeyValue)) {
-
-                    result = this._mapPickArrays(outputKeyValue, oneTemplateObject);
-                } else {
-
-                    result[oneTemplateObject.key] = this.pickResult(outputKeyValue, oneTemplateObject.fields);
-                }
-            } else {
-
-                _.set(result, oneTemplateObject, outputKeyValue);
-            }
-        }, this);
-
-        return result;
-    },
-
-    /**
-     * System func for pickResult.
-     *
-     * @param mapValue
-     * @param templateObject
-     * @returns {*}
-     * @private
-     */
-    _mapPickArrays: function (mapValue, templateObject) {
-
-        var arrayResult = [],
-            result = templateObject.key? {} : [];
-
-        _.map(mapValue, function (inOutArrayValue) {
-
-            arrayResult.push(this.pickResult(inOutArrayValue, templateObject.fields));
-        }, this);
-
-        if (templateObject.key) {
-
-            result[templateObject.key] = arrayResult;
-        } else {
-
-            result = arrayResult;
-        }
-
-        return result;
-    },
-
-    /**
-     * Set acess token.
-     *
-     * @param dexter
-     */
-    authParams: function (dexter) {
-
-        if (dexter.environment('instagram_access_token')) {
-
-            ig.use({access_token: dexter.environment('instagram_access_token')});
-        } else {
-
-            this.fail('A [instagram_access_token] environment is Required.');
-        }
-    },
-
-    prepareStringInputs: function (inputs) {
-        var result = {};
-
-        _.map(inputs, function (inputValue, inputKey) {
-
-            result[inputKey] = _(inputValue).toString();
-        });
-
-        return result;
-    },
-
     /**
      * The main entry point for the Dexter module
      *
@@ -115,20 +20,18 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var credentials = dexter.provider('instagram').credentials(),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        var userId = step.input('userId').first() || 'self';
+        // check params.
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-        this.authParams(dexter);
+        instagram.use({ access_token: _.get(credentials, 'access_token') });
+        instagram.user_follows(inputs.userId || 'self', function (error, result) {
 
-        ig.user_follows(userId, function (err, result) {
-
-            if (err) {
-
-                this.fail(err);
-            } else {
-
-                this.complete(this.pickResult({users: result}, globalPickResult));
-            }
+            error? this.fail(error) : this.complete(util.pickOutputs({ data: result }, pickOutputs));
         }.bind(this));
     }
 };
